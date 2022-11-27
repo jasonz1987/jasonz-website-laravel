61ae24ab4da32982b1850a10362c27916317e405
Jason.z <zzz131519@gmail.com>
2022-11-27 17:45:59 +0800

    修改富文本编辑器~

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -30,24 +30,24 @@ Editor::resolving(function (Form\Field\Editor $editor) {
     $editor->options([
         'setup' => \Dcat\Admin\Support\JavaScript::make(
             <<<JS
-                    function (editor) {
-                        tinymce.init({
-                             codesample_languages: [
-                                {text: 'HTML/XML', value: 'markup'},
-                                {text: 'JavaScript', value: 'javascript'},
-                                {text: 'CSS', value: 'css'},
-                                {text: 'PHP', value: 'php'},
-                                {text: 'Ruby', value: 'ruby'},
-                                {text: 'Python', value: 'python'},
-                                {text: 'Java', value: 'java'},
-                                {text: 'C', value: 'c'},
-                                {text: 'C#', value: 'csharp'},
-                                {text: 'C++', value: 'cpp'}
-                                {text: 'BASH', value: 'bash'}
-                            ],
-                        });
-                    }
-                    JS
+            function (editor) {
+                tinymce.init({
+                     codesample_languages: [
+                        {text: 'HTML/XML', value: 'markup'},
+                        {text: 'JavaScript', value: 'javascript'},
+                        {text: 'CSS', value: 'css'},
+                        {text: 'PHP', value: 'php'},
+                        {text: 'Ruby', value: 'ruby'},
+                        {text: 'Python', value: 'python'},
+                        {text: 'Java', value: 'java'},
+                        {text: 'C', value: 'c'},
+                        {text: 'C#', value: 'csharp'},
+                        {text: 'C++', value: 'cpp'}
+                        {text: 'BASH', value: 'bash'}
+                    ],
+                });
+            }
+            JS
         ),
     ]);
 });
